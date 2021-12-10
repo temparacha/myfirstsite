05e0beec7a84a9e32e6c118c2ca55556742d6b6b
Zhenya Komarov <93269650+temparacha@users.noreply.github.com>
2021-12-10 13:16:57 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+#from . import views
+from .views import HomeView, ArticleDetailView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path ('', include('theblog.urls'))
+    #path('', views.home, name="home"),
+    path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 ]
